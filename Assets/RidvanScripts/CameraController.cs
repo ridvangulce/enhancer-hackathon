234e997704e76d50dd8bfb62c6c5d0f7559dbf8c
ridvangulce <ridvangulce@gmail.com>
2023-04-28 21:53:34 +0300

    Cinemachine and First Scene

---
+++ Assets/RidvanScripts/CameraController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("isStart", true);
+        }
+    }
+}
