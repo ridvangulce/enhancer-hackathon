a52b78e961b58d701b6570d48a4cba35001d8a12
ridvangulce <ridvangulce@gmail.com>
2023-04-29 07:01:39 +0300

    +added Credits menu, credits animation

--- Assets/RidvanScripts/UIController.cs
+++ Assets/RidvanScripts/UIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.RidvanScripts
@@ -6,11 +7,29 @@ namespace Assets.RidvanScripts
     {
         [SerializeField] private CameraController _cameraController;
         [SerializeField] private GameObject _panel;
+        [SerializeField] private GameObject _creditsPanel;
+        [SerializeField] private Transform _creditsTextContainer;
 
         public void StartGame()
         {
             _panel.SetActive(false);
             StartCoroutine(_cameraController.Timer());
+        }
+
+        public void Credits()
+        {
+            StartCoroutine(ActiveCredits());
+        }
+
+        IEnumerator ActiveCredits()
+        {
+            _creditsPanel.SetActive(true);
+            Animator anim = _creditsTextContainer.GetComponent<Animator>();
+            anim.SetBool("isCredit", true);
+            yield return new WaitForSeconds(11f);
+            _creditsPanel.SetActive(false);
+
+            
         }
     }
 }
